8a5d8c2db65b44f5d150fa0bbcc2b63c6ed26ca7
Nordask <solomon7dun@gmail.com>
2024-06-27 22:37:42 +0300

    main: update tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -17,11 +17,8 @@ const PAGE = '/courses';
 const PAGE_URL = `${BASE_URL}${PAGE}`;
 const ADDITIONAL_FILES_FOLDER = 'ru-hexlet-io-courses_files';
 const MAIN_FILE_NAME = 'ru-hexlet-io-courses.html';
-
-const JSON_FILE_NAME = 'ru-hexlet-io-manifest.json';
-const SCRIPT_FILE_NAME = 'ru-hexlet-io-courses';
-const pageName = 'ru-hexlet-io-courses';
-const ext = '.html';
+const MANIFEST_FILE_NAME = 'ru-hexlet-io-manifest.json';
+const CANONICAL_FILE_NAME = 'ru-hexlet-io-courses.html';
 
 let tmpDirPath = '';
 
@@ -30,32 +27,27 @@ describe('pageLoading', () => {
     tmpDirPath = await fsp.mkdtemp(path.join(os.tmpdir(), 'page-loader-'));
   });
   it('should loading file to output', async () => {
-    // console.log('qq111111111111', getFixturePath('test_page.html'), directoryName);
-    const MAIN_FILE = readFixture('ru-hexlet-io-courses.html');
-    const MANIFEST_FILE = readFixture('ru-hexlet-io-courses_files', 'ru-hexlet-io-manifest.json');
-    const ADDITIONAL_FILE = readFixture('ru-hexlet-io-courses_files', 'ru-hexlet-io-courses.html');
+    const MAIN_FILE = readFixture(MAIN_FILE_NAME);
+    const MANIFEST_FILE = readFixture(ADDITIONAL_FILES_FOLDER, MANIFEST_FILE_NAME);
+    const ADDITIONAL_FILE = readFixture(ADDITIONAL_FILES_FOLDER, CANONICAL_FILE_NAME);
 
-    console.log('qq11111111', MANIFEST_FILE);
     nock(BASE_URL)
       .get(PAGE)
       .reply(200, MAIN_FILE)
-      .get('/ru-hexlet-io-courses_files/ru-hexlet-io-manifest.json')
+      .get(`/${ADDITIONAL_FILES_FOLDER}/${MANIFEST_FILE_NAME}`)
       .reply(200, MANIFEST_FILE)
-      .get('/ru-hexlet-io-courses_files/ru-hexlet-io-manifest')
+      .get(`/${ADDITIONAL_FILES_FOLDER}/${CANONICAL_FILE_NAME}`)
       .reply(200, ADDITIONAL_FILE);
+
     await pageLoader(PAGE_URL, tmpDirPath);
 
     const expectedPage = await readFixture('expected.html');
-    const actualPage = await fsp.readFile(path.resolve(tmpDirPath, 'ru-hexlet-io-courses.html'));
-    // const expectedManifestPage = await readFixture('expected.html');
-    const q = await fsp.readdir(path.resolve(tmpDirPath, 'ru-hexlet-io-courses_files'));
-    console.log('qq3333333333333333333', q);
-    const actualManifestPage = await fsp.readFile(path.resolve(tmpDirPath, 'ru-hexlet-io-courses_files', 'ru-hexlet-io-ru-hexlet-io-courses-files-ru-hexlet-io-manifest.json'), 'utf8');
+    const actualPage = await fsp.readFile(path.resolve(tmpDirPath, MAIN_FILE_NAME), 'utf8');
+    const actualManifestPage = await fsp.readFile(path.resolve(tmpDirPath, ADDITIONAL_FILES_FOLDER, 'ru-hexlet-io-ru-hexlet-io-courses-files-ru-hexlet-io-manifest.json'), 'utf8');
+    const actualAdditionalPage = await fsp.readFile(path.resolve(tmpDirPath, ADDITIONAL_FILES_FOLDER, 'ru-hexlet-io-ru-hexlet-io-courses-files-ru-hexlet-io-courses.html'), 'utf8');
 
+    expect(actualPage).toEqual(expectedPage);
     expect(actualManifestPage).toEqual(MANIFEST_FILE);
-
-    
-
-    // expect(actualPage).toEqual(expectedPage);
+    expect(actualAdditionalPage).toEqual(ADDITIONAL_FILE);
   });
 });
